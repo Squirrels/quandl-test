563878d41833dddb8f5ce34c86fbd5076444b0cd
Squirrels <squirrelmcnutsy@gmail.com>
2015-10-06 15:21:35 -0300

    Dataset information successfully being obtained from the API and being added to the local database.

--- tarea-cumplo.js
+++ tarea-cumplo.js
@@ -7,6 +7,9 @@ Databases = new Mongo.Collection("databases");
 Datasets = new Mongo.Collection("datasets");
 
 
+//The data associated with a dataset
+Data = new Mongo.Collection("data");
+
 
 if (Meteor.isClient) {
   Template.body.events({
@@ -20,17 +23,24 @@ if (Meteor.isClient) {
 }
 
 
+//Server variables
+//These are obtained from the settings file
+var apiKey;
+var databaseCode;
+var rateLimit10Minutes;
+var rateLimitDay;
+//Liks to get the data from
+var databaseAPIUrl;
+var datasetMetadataAPIUrl;
+var datasetDataAPIUrl;
+
 
 //Server logic
 if (Meteor.isServer) {
-  //When the server starts, we make the initial check for the database we'll be working with
+  
   Meteor.startup(function () {
-    //Get the values from the configuration file (stored in server/settings.json)
-    //- The Quandl API key
-    var apiKey = Meteor.settings.quandlAPIKey;
-    //- The name of the database the application will be using
-    var databaseCode = Meteor.settings.databaseCode;
-
+    //When the server starts, we load up the configuration from the file server/settings.json
+    Meteor.call("loadServerSettings");
     //Now we check if there's a Database in our server with that name
     if(Databases.find({code: databaseCode}).count() == 0){
       //There are none, we need to add it to our local database and then read the CSV with the datasets for it
@@ -64,45 +74,143 @@ if (Meteor.isServer) {
       console.log("Database already added.");
     }
 
+
     //After checking the database, time to check the dataset
     //We start by checking if the number stored matches the one we got from the server
-    console.log("Datasets " + Datasets.find({database: databaseCode}).count());
-    console.log("Database " + Databases.findOne({code: databaseCode}).datasets);
     if(Datasets.find({database: databaseCode}).count() != Databases.findOne({code: databaseCode}).datasets){
-        console.log("Missing!");
+       //We need to download and process the CSV
+       //To do this we simply call the addDatasets method
+       Meteor.call("addDatasets", databaseCode);
+       Meteor.call("getDatasetInformation", databaseCode, "ZBH");
+
     }
 
-    /*
-    //After adding the database, we need to get the datasets
-    //First we get the number of datasets for this database code
-    var numberOfDatasets = Datasets.find({databaseCode: databaseCode}).count();
-    //Now, if the amount of datasets doesn't match the number we're expecting, it's time to load it from the csv
-    if(numberOfDatasets != 3000){
-      
-    }
+    //Finally, get the dataset's data
+    //Meteor.call("addDatasetData", "WIKI", "AAPL");
+    
+  });
+}
 
-    //Read the csv
-    if(Datasets.find().count() != 0){
-      Assets.getText('WIKI-datasets-codes.csv', function(err, csvText) {
+
+//Methods
+Meteor.methods({
+  //Startup methods
+  /**
+   * Loads the configuration settings from the file server/settings.json
+   */
+  loadServerSettings: function(){
+    console.log("- Loading server's settings -");
+    //- The Quandl API key
+    apiKey = Meteor.settings.quandlAPIKey;
+    console.log("\t-API key loaded.");
+    //- The name of the database the application will be using
+    databaseCode = Meteor.settings.databaseCode;
+    console.log("\t-Database code loaded.");
+    //-The rate limit per 10 minutes
+    rateLimit10Minutes = Meteor.settings.rateLimit10Minutes;
+    console.log("\t-Rate per 10 minutes is : "+rateLimit10Minutes);
+    //-The daily rate limit
+    rateLimitDay = Meteor.settings.rateLimitDay;
+    console.log("\t-Daily rate limit is : "+rateLimitDay);
+    //Now get the links for the API
+    //The link used to get the database information
+    databaseAPIUrl = Meteor.settings.databaseAPIUrl;
+    //The link used to get a dataset's metadata
+    datasetMetadataAPIUrl = Meteor.settings.datasetMetadataAPIUrl;
+    //The link used to get the dataset's data
+    datasetDataAPIUrl = Meteor.settings.datasetDataAPIUrl;
+    //
+    console.log("\t-Links loaded.");
+    //Everything was loaded correctly
+    console.log("- Server's settings loaded correctly - ");
+
+  },
+  addExample: function () {
+
+    /*if(Datasets.find({$and: [{ database: "WOLO" },{ code: 123 }]}).count() == 0){       
+        Datasets.insert({
+            database: "WOLO",
+            name: ":O",
+            code: 123,
+            createdAt: new Date(),
+            lastUpdate: new Date()
+        });*/
+      Meteor.call("updateDataset", "WOLO", 123);
+      console.log("Updated.");
+      console.log(Datasets.find().count());
+    //}
+    //else{
+     // console.log("Duplicated");
+    //}    
+  },
+  //Quandl API call methods
+
+  /**
+   * Makes an http request to the given URL and returns the result.
+   * @param  {String} url The URL to send the http request to.
+   * @return {Object}     The response to the request. It can either be a JSON or an error.
+   */
+  httpRequest: function(url){
+    //We wrap this in a try/catch block to catch error 400 and when the Quandl API may be down
+    try{
+      return Meteor.http.get(url);
+    }
+    catch(error){
+      //We log the error into the console and return null
+      console.log("Error: "+error);
+      return null;
+    }
+    
+  },
+  /**
+   * Gets the information about a database from the Quandl API, given a specific database code.
+   * @param  {String} databaseCode The databases code we're asking information about.
+   * @return {JSON}                The object that contains the result of the query.
+   */
+  getDatabaseInformationJson: function(databaseCode){
+    return Meteor.call("httpRequest", Meteor.call("parseAPIURL", databaseAPIUrl, databaseCode, datasetCode));
+  },
+  /**
+   * Downloads the .zip file that contains the .csv file with all the database's datasets from the Quandl server.
+   * @param  {String} databaseCode The database we're getting the dataset's code.
+   * @return {String}              The name of the csv file.
+   */
+  downloadDatasetsCSV: function(databaseCode){
+      return 'WIKI-datasets-codes.csv';
+  },
+  //
+  /**
+   * Method that downloads and processes the dataset file, adding them to the database.
+   * @param  {String} databaseCode The database we're getting the dataset's code from.
+   * @return {Number}              The number of datasets we added.
+   */
+  addDatasets: function(databaseCode){
+      //First we download and extract the csv, using the method for it
+       var csvFile = Meteor.call("downloadDatasetsCSV", databaseCode);
+
+      //Then we read it, line by line, making sure we don't have any repeated entries
+      //Note that this works asyn
+       Assets.getText(csvFile, function(err, csvText) {
         //Split the csv in the line breaks
         var allTextLines = csvText.split(/\r\n|\n/);
+        //Now create the variables we'll be using for each dataset.
+        //Where we will be storing the parts of each line that makes up a dataset.
         var lineSubElements = [];
+        //The name of the dataset we're currently working with.
         var datasetName;
+        //The counters of datasets read and added.
         var totalRead = 0;
         var totalAdded = 0;
+
         var limiter = 20;
         //for(var i=0; i<allTextLines.length; i++){
         for(var i=10; i<limiter; i++){
-          //We use the greedy operator to split it up to the first ,
+          //We use the greedy operator to split it up to the first , (since the name of the datasets usually have commas too).
           lineSubElements = allTextLines[i].split(/,(.+)?/);
           //Now we have in [0] = DATABASE NAME/DATASET NAME, let's split that by the /
           datasetCode = lineSubElements[0].split('/')[1];
-          //console.log(datasetName);
-          //console.log(lineSubElements[1]);
-          
-
           //Check that it's not repeated before adding it
-          if(Datasets.find({$and: [{ database: databaseName },{ code: datasetCode }]}).count() == 0){
+          if(Datasets.find({$and: [{ databaseCode: databaseCode },{ code: datasetCode }]}).count() == 0){
             //Not added yet, add it.
             Datasets.insert({
               databaseCode: databaseCode,
@@ -116,60 +224,73 @@ if (Meteor.isServer) {
 
           totalRead++;
         }
+        //Log our work.
         console.log("Completed: Read " + totalRead + " datasets. Total added: "+totalAdded);
-
-          var datas = Datasets.find();
-      for(var j=0; j <datas.length; j++){
-        console.log(datas[j]);
-      }
-
-
+        //Finally, we return the total of entries added
+        return totalAdded;
       });
-    }
-    else{
-      console.log("Dataset already loaded.");
-    }*/
-  });
-}
-
-
-//Methods
-Meteor.methods({
-  addExample: function () {
-
-    if(Datasets.find({$and: [{ database: "WOLO" },{ code: 123 }]}).count() == 0){       
-        Datasets.insert({
-            database: "WOLO",
-            name: ":O",
-            code: 123,
-            createdAt: new Date(),
-            lastUpdate: new Date()
-        });
-      console.log("Added.");
-      console.log(Datasets.find().count());
-    }
-    else{
-      console.log("Duplicated");
-    }    
   },
-  //Quandl API call methods
-
-  //Makes an http request to the given URL and returns the result
-  httpRequest: function(url){
-    //We wrap this in a try/catch block to catch error 400 and when the Quandl API may be down
-    try{
-      return Meteor.http.get(url);
+  getDatasetInformation: function(databaseCode, datasetCode){
+      //First check if there's such dataset  
+      var dataset = Datasets.findOne( {$and: [{ databaseCode: databaseCode },{ code: datasetCode }]} );
+      if(dataset != undefined){
+        //Connect to the API using the given values
+        var datasetInfo = Meteor.call("httpRequest", Meteor.call("parseAPIURL", datasetMetadataAPIUrl, databaseCode, datasetCode));
+        if(datasetInfo){
+            
+        }
+        else{
+          return null;
+        }
+      }
+      else{
+        //There's no dataset with that code in that database, return null
+        return null;
+      }
+  },
+  updateDataset: function(databaseCode, datasetCode){
+    var dataset= Datasets.findOne({code: datasetCode});
+    if(dataset.description != undefined){
+      console.log("Description already present");
     }
-    catch(error){
-      //We log the error into the console and return null
-      console.log("Error: "+error);
-      return null;
+    else{
+      Datasets.update(dataset._id, { $set: { description: "What!"} });
+      console.log("Description added.");
     }
     
   },
-  //Gets the information about the database from the quandl api, given a specific database code
-  getDatabaseInformationJson: function(databaseCode){
-    return Meteor.call("httpRequest","https://www.quandl.com/api/v3/databases/"+databaseCode+".json");
+  /**
+   * Connects to the Quandl API and downloads the data for a given dataset. It then adds that to the local database.
+   * @param  {String} databaseCode The database that contains the dataset's code.
+   * @param  {String} datasetCode  The dataset's code.
+   */
+  addDatasetData: function(databaseCode, datasetCode){
+
+      //Connect to the api using the API key
+      var datasetData = Meteor.call("httpRequest", Meteor.call("parseAPIURL", datasetDataAPIUrl, databaseCode, datasetCode)+ "?start_date=2015-05-26&end_date=2015-05-27");
+      //It's a json, so we can iterate through it normally
+      //But first check if there were no errors
+      if(datasetData){
+        //Get the columns
+        console.log(datasetData.data.dataset_data.column_names);
+        console.log(datasetData.data.dataset_data.data);
+      }
+  },
+ /**
+  * This method places the correct data in a given link, replacing it. 
+  * @param  {String} url          The url to put the information itn.
+  * @param  {String} databaseCode The database's code.
+  * @param  {String} datasetCode  The dataset's code. Can be empty.
+  * @return {String}              The url with the correct data.
+  */
+  parseAPIURL: function(url, databaseCode, datasetCode){
+    console.log(url);
+    //First we replace the database code
+    var result = url.replace(":databaseCode",databaseCode);
+    //Then the dataset code
+    result = result.replace(":datasetCode",datasetCode);
+    //Now we return it
+    return result;
   }
   //Download profile image
 
